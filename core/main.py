a5bcbca457d4cbe4f87baa5c12941ac486fde73d
ded2322 <151318767+ded2322@users.noreply.github.com>
2024-06-07 11:23:24 +0700

    v.1.0

--- core/main.py
+++ core/main.py
@@ -7,11 +7,11 @@ app = FastAPI(title="User service", version='1.0')
 
 app.include_router(router)
 
-origins = ["localhost"]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
